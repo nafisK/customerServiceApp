578dcefddfb4bf5fbd63a7197b64c37fe9d421ba
Nafis Khan <nafisrizwank@gmail.com>
2022-09-02 13:18:53 -0400

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Admin from './Pages/Admin'
+import User from './Pages/User'
+import Login from './Pages/Login'
 
 function App() {
-  return <div className='App'>Hello World</div>
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Admin />} />
+        <Route path='/admin' element={<Admin />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/user' element={<User />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
