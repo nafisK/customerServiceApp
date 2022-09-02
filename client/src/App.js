0ffc7567e3552241d03da2120345f81d8e6f2cda
Nafis Khan <nafisrizwank@gmail.com>
2022-09-02 13:20:25 -0400

    added 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Admin from './Pages/Admin'
 import User from './Pages/User'
 import Login from './Pages/Login'
+import Error from './Pages/Error'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/admin' element={<Admin />} />
         <Route path='/login' element={<Login />} />
         <Route path='/user' element={<User />} />
+        <Route path='/*' element={<Error />} />
       </Routes>
     </Router>
   )
